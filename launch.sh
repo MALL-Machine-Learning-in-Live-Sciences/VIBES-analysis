b54519a458cb81f0d375453c2dcf896650f68eed
DiegoFE94 <diegolas94@hotmail.com>
2021-04-26 17:45:55 +0200

    launch edit

--- launch.sh
+++ launch.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=16G
 
 module load gcc/6.4.0 R/4.0.2
-TMP=/mnt/netapp2/Store_uni/home/ulc/co/dfe/TEMPR Rscript /home/ulc/co/dfe/git/Entropy/TaxaAcquisition2.R
+TMP=/mnt/netapp2/Store_uni/home/ulc/co/dfe/TEMPR Rscript /home/ulc/co/dfe/git/Entropy/MachineLearning.R
