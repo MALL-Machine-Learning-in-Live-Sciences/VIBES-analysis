26f92a20b95a023c2f7adb1ed9ac6cc21f0ae431
DiegoFE94 <diegolas94@hotmail.com>
2021-04-27 18:04:02 +0200

    .

--- functions/FunctionsML.R
+++ functions/FunctionsML.R
@@ -63,7 +63,7 @@ ML.exec = function(dataset){
   l5 = makeLearner("classif.gbm", predict.type = "prob")
   lrn_GBM =  makeTuneWrapper(learner = l5, resampling = inner, measures = auc, par.set = psGBM, control = ctrl, show.info = T)
   
-  learners = list(lrn_RF, lrn_glmnet, lrn_KSVM, lrn_GB, lrn_GBM)
+  learners = list(lrn_RF, lrn_glmnet, lrn_GB, lrn_KSVM, lrn_GBM)
   #learners = (lrn_GB)
   # Outer
   outer = makeResampleDesc('RepCV' , reps = 5, folds = 3 , stratify = T)
