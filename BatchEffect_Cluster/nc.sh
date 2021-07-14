770b51d93be1a3930959b6564a8d3e5fd52a4666
DiegoFE94 <diegolas94@hotmail.com>
2021-07-14 10:22:07 +0200

    add sh

---
+++ BatchEffect_Cluster/nc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p thinnodes
+#SBATCH -t 00:30:00
+#SBATCH --mem=16G
+#SBATCH --error="/mnt/netapp2/Store_uni/home/ulc/co/dfe/projects/Entropy/data/NC_error.txt"
+#SBATCH --output="/mnt/netapp2/Store_uni/home/ulc/co/dfe/projects/Entropy/data/NC_salida_R.txt"
+
+module load gcc/6.4.0 R/4.0.2
+TMP=/mnt/netapp2/Store_uni/home/ulc/co/dfe/TEMPR Rscript /home/ulc/co/dfe/git/Entropy/BatchEffect_Cluster/NO_CLUST_MMPUHI.R
