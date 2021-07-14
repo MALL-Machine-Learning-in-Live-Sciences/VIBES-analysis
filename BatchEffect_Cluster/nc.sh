fcbcbb5cb4ab8fbb7c709cc48efb94459a6d02e2
DiegoFE94 <diegolas94@hotmail.com>
2021-07-14 11:02:41 +0200

    NC

--- BatchEffect_Cluster/nc.sh
+++ BatchEffect_Cluster/nc.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1
 #SBATCH -p thinnodes
 #SBATCH -t 00:30:00
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --error="/mnt/netapp2/Store_uni/home/ulc/co/dfe/projects/Entropy/data/NC_error.txt"
 #SBATCH --output="/mnt/netapp2/Store_uni/home/ulc/co/dfe/projects/Entropy/data/NC_salida_R.txt"
 
